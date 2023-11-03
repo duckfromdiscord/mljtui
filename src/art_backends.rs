07fe037f1a8260437ca14a9f199e7bd0487844b2
duck <113956421+duckfromdiscord@users.noreply.github.com>
2023-11-03 16:17:22 -0400

    album art displays. need to dynamically size

---
+++ src/art_backends.rs
@@ -0,0 +1,110 @@
+use crossbeam::channel::Sender;
+use image::DynamicImage;
+use mljcl::{types::Album, MalojaCredentials};
+use rascii_art::RenderOptions;
+
+use bytes::Bytes;
+
+#[derive(Clone)]
+pub struct CachedRender {
+    pub art: String,
+    pub size: (u32, u32),
+}
+
+#[derive(Clone)]
+pub struct AlbumArt {
+    pub album_id: String,
+    pub name: String,
+    pub art: Option<CachedRender>,
+    pub image: DynamicImage,
+}
+
+pub fn truncate(string: String, max_len: usize) -> String {
+    if string.len() <= max_len {
+        string
+    } else {
+        string.split_at(max_len - 1).0.to_owned() + &"…"
+    }
+}
+
+impl AlbumArt {
+    pub fn display_string(&self) -> String {
+        match &self.art {
+            Some(render) => {
+                render.art.clone()
+                    + "\n"
+                    + &truncate(self.clone().name, render.size.1.try_into().unwrap())
+            }
+            None => "".to_string(),
+        }
+    }
+    #[allow(dead_code)]
+    pub fn display_string_with_size(&mut self, height: u32, width: u32) -> String {
+        match self.art.clone() {
+            Some(render) => {
+                if height == 1 {
+                    return truncate(self.clone().name, width.try_into().unwrap());
+                }
+                if !(render.size == (height, width)) {
+                    self.art = Some(CachedRender {
+                        art: rascii(self.image.clone(), height, width),
+                        size: (height, width),
+                    });
+                }
+                self.display_string()
+            }
+            None => "".to_string(),
+        }
+    }
+}
+
+pub fn get_image(data: Bytes) -> Option<DynamicImage> {
+    if let Ok(img) = image::load_from_memory(&data) {
+        return Some(img);
+    } else {
+        return None;
+    }
+}
+
+pub fn rascii(image: DynamicImage, height: u32, width: u32) -> String {
+    let mut ret: String = "".to_string();
+    let render_options = RenderOptions::new()
+        .charset(rascii_art::charsets::BLOCK)
+        .colored(true)
+        .height(height)
+        .width(width);
+    let _ = rascii_art::render_image_to(&image, &mut ret, &render_options);
+    ret
+}
+
+pub async fn get_art_for(
+    album: (Album, u64),
+    sender: Sender<AlbumArt>,
+    credentials: MalojaCredentials,
+    client: reqwest::Client,
+) {
+    let id = album.0.id;
+    let bytes = mljcl::art::album_art_async(id.clone(), credentials, client)
+        .await
+        .unwrap();
+    match get_image(bytes) {
+        Some(image) => {
+            let bytes = rascii(image.clone(), 8, 16);
+            sender
+                .send(AlbumArt {
+                    album_id: id,
+                    art: Some(CachedRender {
+                        art: bytes,
+                        size: (8, 16),
+                    }),
+                    name: album.0.name,
+                    image,
+                })
+                .expect("Error sending Album Art between threads");
+            drop(sender);
+        }
+        None => {
+            drop(sender);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,115 @@
-use mljcl::{MalojaCredentials, history::{Scrobble, scrobbles}};
+use ansi_to_tui::IntoText;
 use clap::{Arg, Command};
-use std::io::{self, stdout};
+use crossbeam::channel::unbounded;
 use crossterm::{
     event::{self, Event, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
     ExecutableCommand,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen}
+};
+use mljcl::{
+    charts,
+    history::{scrobbles_async, Scrobble},
+    range::Range,
+    MalojaCredentials,
 };
 use ratatui::{prelude::*, widgets::*};
+use std::{
+    io::{self, stdout},
+    time::Duration,
+};
+
+mod art_backends;
+
+use crate::art_backends::*;
+
+struct AlbumCharts {
+    albums: Vec<(String, String, u64, Option<AlbumArt>)>, // ID, Name, Rank
+}
 
 struct App {
     items: Vec<Scrobble>,
+    albums: AlbumCharts,
+    receiver: crossbeam::channel::Receiver<AlbumArt>,
 }
 
-pub fn main() -> io::Result<()> {
+#[tokio::main]
+pub async fn main() -> io::Result<()> {
     let matches = Command::new("mljtui")
-    .arg(
-        Arg::new("ip")
-        .short('i')
-        .value_name("IP")
-        .help("Maloja API IP")
-    )
-    .arg(
-        Arg::new("port")
-        .short('p')
-        .value_name("PORT")
-        .help("Maloja API Port")
-    )
-    .arg(
-        Arg::new("https")
-        .short('s')
-        .value_name("HTTPS")
-        .help("Whether to use HTTPS")
-        .num_args(0)
-    )
-    .get_matches();
+        .arg(
+            Arg::new("ip")
+                .short('i')
+                .value_name("IP")
+                .help("Maloja API IP"),
+        )
+        .arg(
+            Arg::new("port")
+                .short('p')
+                .value_name("PORT")
+                .help("Maloja API Port"),
+        )
+        .arg(
+            Arg::new("https")
+                .short('s')
+                .value_name("HTTPS")
+                .help("Whether to use HTTPS")
+                .num_args(0),
+        )
+        .get_matches();
 
     let creds = MalojaCredentials {
         https: matches.get_flag("https"),
         skip_cert_verification: matches.get_flag("https"),
-        ip: matches.get_one::<String>("ip").expect("IP required").to_string(),
-        port: matches.get_one::<String>("port").unwrap_or(&"42010".to_string()).parse::<u16>().expect("Port must be an integer between 1-65535"),
+        ip: matches
+            .get_one::<String>("ip")
+            .expect("IP required")
+            .to_string(),
+        port: matches
+            .get_one::<String>("port")
+            .unwrap_or(&"42010".to_string())
+            .parse::<u16>()
+            .expect("Port must be an integer between 1-65535"),
         api_key: None,
     };
 
-    let mut recent_scrobbles = scrobbles(None, mljcl::range::Range::AllTime, Some(0), Some(30), creds).unwrap();
+    let client = mljcl::get_client_async(&creds);
+    let mut recent_scrobbles = scrobbles_async(
+        None,
+        Range::AllTime,
+        Some(0),
+        Some(30),
+        creds.clone(),
+        client.clone(),
+    )
+    .await
+    .unwrap();
     recent_scrobbles.reverse();
+
+    let (s, r) = unbounded();
+
+    let mut album_charts =
+        charts::charts_albums_async(Range::AllTime, None, creds.clone(), client.clone())
+            .await
+            .unwrap();
+    let mut albums: Vec<(String, String, u64, Option<AlbumArt>)> = vec![];
+
+    album_charts.albums.truncate(4);
+    for album in album_charts.albums {
+        albums.push((album.clone().0.id, album.clone().0.name, album.1, None));
+        let album_client = client.clone();
+        let album_creds = creds.clone();
+        let s_clone = s.clone();
+        tokio::spawn(async move {
+            art_backends::get_art_for(album, s_clone, album_creds, album_client).await;
+        });
+    }
 
     enable_raw_mode()?;
     stdout().execute(EnterAlternateScreen)?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
     let mut app = App {
         items: recent_scrobbles,
+        albums: AlbumCharts { albums },
+        receiver: r,
     };
     let mut should_quit = false;
     while !should_quit {
@@ -69,7 +128,7 @@ fn handle_events() -> io::Result<bool> {
             if key.kind == event::KeyEventKind::Press && key.code == KeyCode::Char('q') {
                 return Ok(true);
             }
-       }
+        }
     }
     Ok(false)
 }
@@ -89,48 +148,101 @@ fn ui(frame: &mut Frame, app: &mut App) {
         .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
         .split(main_layout[1]);
 
-
     frame.render_widget(
         Block::default().borders(Borders::ALL).title("Charts"),
         halves[0],
     );
 
-    let history_layout = Layout::default()
+    let chart_sections = Layout::default()
         .direction(Direction::Vertical)
         .constraints([
-            Constraint::Percentage(50),
-            Constraint::Percentage(50),
+            Constraint::Percentage(33),
+            Constraint::Percentage(33),
+            Constraint::Percentage(34),
         ])
-        .split(halves[1]);
-    
-    
+        .split(halves[0]);
+
+    let new_sec = ratatui::layout::Rect::new(
+        chart_sections[0].x + 1,
+        chart_sections[0].y + 1,
+        chart_sections[0].width - 2,
+        chart_sections[0].height + 1,
+    );
 
     frame.render_widget(
-        Block::default().borders(Borders::ALL).title("Last Scrobbles"),
+        Block::default().borders(Borders::ALL).title(format!("Top Albums {}/4", app.albums.albums.len())),
+        new_sec,
+    );
+
+    let top_albums = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Percentage(10),
+            Constraint::Percentage(10),
+            Constraint::Percentage(10),
+            Constraint::Percentage(10),
+        ])
+        .split(new_sec);
+
+    let mut album_boxes: Vec<Rect> = vec![];
+
+    for i in 0..=3 {
+        album_boxes.push(ratatui::layout::Rect::new(
+            top_albums[i].x + 1 + (12u16 * i as u16),
+            top_albums[0].y,
+            16,
+            10,
+        ));
+    }
+
+    let mut album_num = 0;
+    for album in album_boxes {
+        let text = match app.albums.albums.get(album_num) {
+            Some(album) => match &album.3 {
+                Some(art) => art.display_string(),
+                None => truncate(album.clone().1, 3),
+            },
+            None => "".to_string(),
+        };
+
+        let text = text.into_text().unwrap();
+        let paragraph = Paragraph::new(text);
+
+        let block = Block::new().borders(Borders::NONE).title("");
+        frame.render_widget(paragraph.clone().block(block), album);
+        album_num += 1;
+    }
+
+    let history_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
+        .split(halves[1]);
+
+    frame.render_widget(
+        Block::default()
+            .borders(Borders::ALL)
+            .title("Last Scrobbles"),
         history_layout[0],
     );
 
-
     let scrobbles: Vec<ListItem> = app
-    .items
-    .iter()
-    .rev()
-    .map(|entry| {
-        // TODO: Change the date into something more like "119 minutes ago"
-        let date: String = format!("{}", entry.time.format("%Y/%d/%m %H:%M"));
-        let header = Line::from(vec![
-            Span::styled(date, Style::new().gray().italic()),
-            "  ".into(),
-            Span::styled(entry.track.artists[0].clone(), Style::new().gray()),
-            " -- ".into(),
-            Span::styled(entry.track.name.clone(), Style::new().white()),
-        ]);
+        .items
+        .iter()
+        .rev()
+        .map(|entry| {
+            // TODO: Change the date into something more like "119 minutes ago"
+            let date: String = format!("{}", entry.time.format("%Y/%d/%m %H:%M"));
+            let header = Line::from(vec![
+                Span::styled(date, Style::new().gray().italic()),
+                "  ".into(),
+                Span::styled(entry.track.artists[0].clone(), Style::new().gray()),
+                " -- ".into(),
+                Span::styled(entry.track.name.clone(), Style::new().white()),
+            ]);
 
-        ListItem::new(vec![
-            header,
-        ])
-    })
-    .collect();
+            ListItem::new(vec![header])
+        })
+        .collect();
 
     let events_list = List::new(scrobbles)
         .block(Block::default().borders(Borders::ALL).title("List"))
@@ -141,7 +253,13 @@ fn ui(frame: &mut Frame, app: &mut App) {
         Block::default().borders(Borders::ALL).title("Pulse"),
         history_layout[1],
     );
-    
 
-    
+    let recv_result = app.receiver.recv_timeout(Duration::from_micros(1000));
+    if let Ok(art) = recv_result {
+        for album in &mut app.albums.albums {
+            if album.0 == art.clone().album_id {
+                album.3 = Some(art.clone());
+            }
+        }
+    }
 }
